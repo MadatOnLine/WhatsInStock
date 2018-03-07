0d36910b52f81d07821f16f7328ce9ca91f57f6f
bud16005 <bud16005@byui.edu>
2018-03-07 16:00:18 -0700

    "We completed the preferences"

--- app/src/main/java/team02/whatsinstock/MainActivity.java
+++ app/src/main/java/team02/whatsinstock/MainActivity.java
@@ -1,23 +1,43 @@
 package team02.whatsinstock;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     // Ingredients should not be searched if none have been selected
-    public List<String> ingredients = null;
-    // Results from using Search should not return zero initially
-    public List<String> results = null;
-    // Searches from the ingredients search bar should not be null
-    public String searchIngredients = null;
+    private List<String> ingredients = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences preferences = getSharedPreferences("SavedIngredients", MODE_PRIVATE);
+        if (preferences.contains("0")) {
+            int i = 0;
+            while (preferences.contains(String.valueOf(i))) {
+                String data = preferences.getString(String.valueOf(i++),"");
+                ingredients.add(data);
+            }
+        }
+        else {
+            ingredients.add("fruit");
+        }
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        SharedPreferences.Editor editor = getSharedPreferences("SavedIngredients", MODE_PRIVATE).edit();
+        int i = 0;
+        for (String data : ingredients) {
+            editor.putString(String.valueOf(i++), data);
+        }
+        editor.apply();
     }
 }
